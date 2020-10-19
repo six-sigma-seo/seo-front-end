47beec1b3fe1a9b184756bbf5cf173455763aa82
Jose Barrios <jbarriospd@gmail.com>
2020-10-19 16:36:00 -0500

    🚧 creating the firts atom(button)

---
+++ src/pages/home/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '../../atoms/button/button'
+
+function Home () {
+    return <Button disabled>Template</Button>;
+  };
+  
+  export default Home;
+
+
