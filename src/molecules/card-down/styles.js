6bdd14f1adcd4da4a17ac92c581fa01b0fd0952f
Jose Barrios <jbarriospd@gmail.com>
2020-12-09 12:15:44 -0500

    [Add] card-info-page

---
+++ src/molecules/card-down/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+  
+  font-family: 'Open Sans', sans-serif;
+  position: relative;
+  background: rgba(255, 255, 255, 0.8);
+  margin: 5px;
+  padding: 15px;
+  max-width: 240px;
+  border-radius: 20px;
+  height: 500px;
+  text-align: center;
+  box-shadow: 0px 20px 40px rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(40px);
+`;
+
+export { CardContainer };
