8a686c80bc5661629de6707c0eda058b5236220c
Jose Barrios <jbarriospd@gmail.com>
2020-12-15 21:52:04 -0500

    [Fix] Carousel features

--- src/organisms/styles.js
+++ src/organisms/styles.js
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 
 const CarouselContainer = styled.section`
   display: flex;
-  /* width: 50vw; */
+  width: 50vw;
   padding: 5px;
   overflow-x: auto;
   overflow-y: hidden;
   margin: 0 auto;
+  margin-bottom: 20px;
   &::-webkit-scrollbar {
     height: 5px;
   }
